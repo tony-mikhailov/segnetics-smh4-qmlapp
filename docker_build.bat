22ca6fcf5089cbcfd4d6293a6745f703730c8797
Anton Mikhailov <anton@mikhailov.center>
2022-01-06 00:03:33 +0300

    add win scripts

---
+++ docker_build.bat
@@ -0,0 +1,1 @@
+docker build -t tonymikhailov/smh4_build_qt5.9.4 .
---
+++ docker_run.bat
@@ -0,0 +1,2 @@
+docker run --rm -it --mount type=bind,source="%cd%",target=/host tonymikhailov/smh4_build_qt5.9.4:latest /make.sh
+
